78a4d1130407eef38359b89d5030e3b53cca45af
Jhonatan <jhonatangomez_18@hotmail.com>
2025-04-02 21:02:23 -0500

    Clases listas

--- Clases/clsCliente.cs
+++ Clases/clsCliente.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Examen2.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,80 @@ namespace Examen2.Clases
 {
 	public class clsCliente
 	{
-	}
+		private DBExamenEntities dbExamen = new DBExamenEntities();
+		public Cliente cliente { get; set; }
+
+        public string Insertar()
+        {
+            try
+            {
+                dbExamen.Clientes.Add(cliente);
+                dbExamen.SaveChanges();
+                return "El cliente se agregó correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "Error al insertar el cliente " + ex.Message;
+            }
+        }
+
+        public Cliente Consultar(string Id_cliente)
+        {
+            Cliente client = dbExamen.Clientes.FirstOrDefault(c => c.Documento == Id_cliente);
+            return client;
+
+        }
+
+        public List<Cliente> ConsultarTodos()
+        {
+            return dbExamen.Clientes.OrderBy(c => c.Nombre).ToList();
+
+        }
+
+        public string Actualizar()
+        {
+            try
+            {
+                Cliente client = Consultar(cliente.Documento);
+                if (client != null)
+                {
+                    dbExamen.Clientes.AddOrUpdate(cliente);
+                    dbExamen.SaveChanges();
+                    return "El cliente se actualizó correctamente ";
+                }
+                else
+                {
+                    return "El cliente no existe ";
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Error al actualizar el cliente " + ex.Message;
+            }
+        }
+        public string Eliminar()
+        {
+            try
+            {
+                Cliente client = Consultar(cliente.Documento);
+                if (client == null)
+                {
+                    return "El cliente no existe ";
+                }
+                else
+                {
+
+                    dbExamen.Clientes.Remove(client);
+                    dbExamen.SaveChanges();
+                    return "El cliente se eliminó correctamente ";
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return "Error al eliminar el cliente " + ex.Message;
+            }
+
+        }
+    }
 }
---
+++ Clases/clsPrenda.cs
@@ -0,0 +1,88 @@
+﻿using Examen2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace Examen2.Clases
+{
+	public class clsPrenda
+	{
+        private DBExamenEntities dbExamen = new DBExamenEntities();
+        public Prenda prenda { get; set; }
+
+        public string Insertar()
+        {
+            try
+            {
+                dbExamen.Prendas.Add(prenda);
+                dbExamen.SaveChanges();
+                return "La prenda se agregó correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "Error al insertar la prenda " + ex.Message;
+            }
+        }
+
+        public Prenda Consultar(int Id_prenda)
+        {
+            Prenda prend = dbExamen.Prendas.FirstOrDefault(p => p.IdPrenda == Id_prenda);
+            return prend;
+
+        }
+
+        public List<Prenda> ConsultarTodos()
+        {
+            return dbExamen.Prendas.OrderBy(p => p.IdPrenda).ToList();
+
+        }
+
+        public string Actualizar()
+        {
+            try
+            {
+                Prenda prend = Consultar(prenda.IdPrenda);
+                if (prend != null)
+                {
+                    dbExamen.Prendas.AddOrUpdate(prenda);
+                    dbExamen.SaveChanges();
+                    return "La prenda se actualizó correctamente ";
+                }
+                else
+                {
+                    return "La prenda no existe ";
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Error al actualizar la prenda " + ex.Message;
+            }
+        }
+        public string Eliminar()
+        {
+            try
+            {
+                Prenda prend = Consultar(prenda.IdPrenda);
+                if (prend == null)
+                {
+                    return "La prenda no existe ";
+                }
+                else
+                {
+
+                    dbExamen.Prendas.Remove(prend);
+                    dbExamen.SaveChanges();
+                    return "La prenda se eliminó correctamente ";
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return "Error al eliminar la prenda " + ex.Message;
+            }
+
+        }
+    }
+}
